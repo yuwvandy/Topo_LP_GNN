36f05c72daf7208bc7ae66b53a6da37bc109f5c2
Yu Wang (Jack) <yu.wang.1@Vanderbilt.Edu>
2024-04-29 02:00:06 -0500

    Update run_collab_gcn.sh

--- TC-expr-collab/run_collab_gcn.sh
+++ TC-expr-collab/run_collab_gcn.sh
@@ -1,3 +1,8 @@
 python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=2 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN' --save --remove_rep --train
 
 python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=2 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN-aug' --save --remove_rep --aug --gcn_denoise --train
+
+
+python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=2 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN' --save --remove_rep --train --use_val
+
+python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=2 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN-aug' --save --remove_rep --aug --gcn_denoise --train --use_val
